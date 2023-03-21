da6156e62a8653ec7fdc42efe710fd954c9b04f5
Aluno <kaua.moreno2005@gmail.com>
2023-03-21 09:27:21 -0300

    function renderiza, passa as informações de um card para outro

--- assets/js/infoProdutos.js
+++ assets/js/infoProdutos.js
@@ -19,3 +19,18 @@ fetch("assets/json/produtos.json").then(response => response.json())
         document.getElementById('card2-descricao').innerHTML = data.produtos[1].descricao;
         document.getElementById('card3-descricao').innerHTML = data.produtos[2].descricao;
     })
+
+function renderiza(produto) {
+
+    fetch("assets/json/produtos.json").then(response => response.json())
+        .then(data => {
+
+            console.log(data);
+            console.log(produto);
+
+            //SETANDO AS INFORMAÇÔES
+            document.getElementById('card-img').src = data.produtos[produto].img;
+            document.getElementById('card-titulo').innerHTML = data.produtos[produto].nome;
+            document.getElementById('card-descricao').innerHTML = data.produtos[produto].descricao;
+        })
+}
