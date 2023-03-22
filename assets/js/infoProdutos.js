4b3446c42d87e2acdcd8fd1ac80eebd41571cfd9
Aluno <kaua.moreno2005@gmail.com>
2023-03-22 10:08:55 -0300

    inicio branch renderizaJs

--- assets/js/infoProdutos.js
+++ assets/js/infoProdutos.js
@@ -1,36 +1,47 @@
-
-fetch("assets/json/produtos.json").then(response => response.json())
-    .then(data => {
-
-        console.log(data);
-
-        //SETANDO IMAGENS
-        document.getElementById('card1-img').src = data.produtos[0].img;
-        document.getElementById('card2-img').src = data.produtos[1].img;
-        document.getElementById('card3-img').src = data.produtos[2].img;
-
-        //SETANDO TITULOS
-        document.getElementById('card1-titulo').innerHTML = data.produtos[0].nome;
-        document.getElementById('card2-titulo').innerHTML = data.produtos[1].nome;
-        document.getElementById('card3-titulo').innerHTML = data.produtos[2].nome;
-
-        //SETANDO DESCRICOES
-        document.getElementById('card1-descricao').innerHTML = data.produtos[0].descricao;
-        document.getElementById('card2-descricao').innerHTML = data.produtos[1].descricao;
-        document.getElementById('card3-descricao').innerHTML = data.produtos[2].descricao;
-    })
-
-function renderiza(produto) {
-
+window.onload = function () {
     fetch("assets/json/produtos.json").then(response => response.json())
         .then(data => {
 
             console.log(data);
-            console.log(produto);
 
-            //SETANDO AS INFORMAÇÔES
-            document.getElementById('card-img').src = data.produtos[produto].img;
-            document.getElementById('card-titulo').innerHTML = data.produtos[produto].nome;
-            document.getElementById('card-descricao').innerHTML = data.produtos[produto].descricao;
+            //SETANDO IMAGENS
+            document.getElementById('card1-img').src = data.produtos[0].img;
+            document.getElementById('card2-img').src = data.produtos[1].img;
+            document.getElementById('card3-img').src = data.produtos[2].img;
+
+            //SETANDO TITULOS
+            document.getElementById('card1-titulo').innerHTML = data.produtos[0].nome;
+            document.getElementById('card2-titulo').innerHTML = data.produtos[1].nome;
+            document.getElementById('card3-titulo').innerHTML = data.produtos[2].nome;
+
+            //SETANDO DESCRICOES
+            document.getElementById('card1-descricao').innerHTML = data.produtos[0].descricao;
+            document.getElementById('card2-descricao').innerHTML = data.produtos[1].descricao;
+            document.getElementById('card3-descricao').innerHTML = data.produtos[2].descricao;
         })
+}
+
+
+function renderiza(produto) {
+
+    window.location.href = 'compraLoja.html';
+
+    window.onload = function () {
+
+        console.log("teste");
+
+        fetch("assets/json/produtos.json").then(response => response.json())
+            .then(data => {
+
+                console.log(data);
+                console.log(produto);
+
+                //SETANDO AS INFORMAÇÔES
+                document.getElementById('img').src = data.produtos[produto].img;
+                document.getElementById('nome').innerHTML = data.produtos[produto].nome;
+                document.getElementById('descricao').innerHTML = data.produtos[produto].descricao;
+
+            })
+
+    }
 }
