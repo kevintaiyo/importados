d34d997ed302efdf97a13297ab0b4d258b968627
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-03-25 20:57:34 -0300

    update branch cep

--- assets/js/cep.js
+++ assets/js/cep.js
@@ -1,13 +1,16 @@
+// CEP PARA TESTES
 // CEP Sul - 83705240
 // CEP Nordeste - 44082400
 // CEP Sudeste - 24855272
 // CEP Centro-Oeste - 70650171
 // CEP Norte - 67013218
 
+//Resgatando o preco em reais do produto pela URL
+const params = new URLSearchParams(window.location.search);
+const precoReaisValor = params.get('precoReais');
+
 function pesquisa() {
     const cep = document.getElementById('cep').value
-    
-    
 
     fetch(`https://viacep.com.br/ws/${cep}/json/`).then(resposta => {
         return resposta.json();
@@ -16,6 +19,7 @@ function pesquisa() {
         console.log(dados);
         var desconto;
 
+        //Verificando a região do cep e aplicando um desconto
         switch (estado) {
 
             //SUDESTE*************************
@@ -25,6 +29,7 @@ function pesquisa() {
             case 'RJ':
                 console.log("Região: SUDESTE - 30% de desconto");
                 desconto = 0.3;
+                break;
 
             //CENTRO-OESTE********************
             case 'MS':
@@ -32,14 +37,16 @@ function pesquisa() {
             case 'GO':
             case 'DF':
                 console.log("Região: CENTRO-OESTE - 10% de desconto");
-                return desconto = 0.1;
+                desconto = 0.1;
+                break;
 
             //SUL*****************************
             case 'PR':
             case 'SC':
             case 'RS':
                 console.log("Região: SUL - 20% de desconto");
-                return desconto = 0.2;
+                desconto = 0.2;
+                break;
 
             //NORTE***************************
             case 'AC':
@@ -50,7 +57,8 @@ function pesquisa() {
             case 'PA':
             case 'TO':
                 console.log("Região: NORTE - 3% de desconto");
-                return desconto = 0.03;
+                desconto = 0.03;
+                break;
 
             //NORDESTE************************
             case 'MA':
@@ -64,31 +72,33 @@ function pesquisa() {
             case 'SE':
                 console.log("Região: NORDESTE - 7% de desconto");
                 return desconto = 0.07;
-
-            default:
-                console.log("CEP inválido");
         }
 
-        console.log(desconto);
-    })
+        //Confirmação da compra - Mostra o valor integral do produto, o desconto e o preco com desconto - Biblioteca SweetAlert
+        Swal.fire({
+            title: 'Confirmação de compra',
+            text: `Pelo seu CEP vimos que você tem direito a ${desconto * 100}% de desconto, sendo o preco de ${precoReaisValor} para ${(precoReaisValor * (1 -desconto)).toFixed(2)}`,
+            icon: 'info',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Confirmar Compra!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                    'Compra Confirmada',
+                    `Você acaba de adquirir o produto ${document.getElementById('nome').value} no valor de  ${(precoReaisValor * (1 - desconto)).toFixed(2)}`,
+                )
+            }
+        })
 
-    
+    }).catch(function (erro) { 
+        //
+        Swal.fire({
+            icon: 'error',
+            title: "CEP não reconhecido",
+            text: 'Insira um cep existente!',
+        })
+        document.getElementById('cep').value = ''
+    });
 }
-
-Swal.fire({
-    title: 'Confirmação de compra',
-    text: `Pelo seu CEP vimos que você tem direito a ${desconto}% de desconto, sendo o preco atual de ${document.getElementById('precoReal').value*desconto}`,
-    icon: 'info',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, delete it!'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire(
-        'Deleted!',
-        'Your file has been deleted.',
-        'success'
-      )
-    }
-  })
--- assets/js/infoProdutos.js
+++ assets/js/infoProdutos.js
@@ -2,9 +2,6 @@ if (window.location.pathname === '/index.html') {
     window.onload = function () {
         fetch("assets/json/produtos.json").then(response => response.json())
             .then(data => {
-
-                console.log(data);
-
                 //SETANDO IMAGENS
                 document.getElementById('card1-img').src = data.produtos[0].img;
                 document.getElementById('card2-img').src = data.produtos[1].img;
@@ -23,13 +20,14 @@ if (window.location.pathname === '/index.html') {
     }
 }
 
+//Recebe o "id" do botão do card pressionado e coloca na url 
 function renderiza(produto) {
-
-    window.location.href = 'compraLoja.html?produto=' + produto;
-
+    window.location.href = 'compraLoja.html?produto=' + produto
 }
 
 if (window.location.pathname === '/compraLoja.html') {
+
+    //Pega o numero do produto escolhido pelo usuario que fica na url
     const params = new URLSearchParams(window.location.search);
     const produto = params.get('produto');
 
@@ -37,21 +35,22 @@ if (window.location.pathname === '/compraLoja.html') {
         fetch("assets/json/produtos.json").then(response => response.json())
             .then(data => {
 
-                console.log(data);
-                console.log(produto);
-
-                //SETANDO AS INFORMAÇÔES
+                //SETANDO AS INFORMAÇÔES ESTÁTICAS DO PRODUTO
                 document.getElementById('img').src = data.produtos[produto].img;
                 document.getElementById('nome').innerHTML = data.produtos[produto].nome;
                 document.getElementById('descricao').innerHTML = data.produtos[produto].descricao;
                 document.getElementById('precoDolar').innerHTML = data.produtos[produto].precoDolar + " USD";
 
-                //SETANDO AS INFORMAÇÔES DE REAIS
+                //SETANDO AS INFORMAÇÔES DE PREÇO REAIS - API
                 fetch(`https://economia.awesomeapi.com.br/json/last/USD-BRL`).then(resposta => {
                     return resposta.json();
                 }).then(economia => {
                     var dolar = economia.USDBRL.bid;
-                    document.getElementById('precoReal').innerHTML = (data.produtos[produto].precoDolar * dolar).toFixed(2) + " BRL";
+                    const precoReais = (data.produtos[produto].precoDolar * dolar).toFixed(2);
+                    document.getElementById('precoReal').innerHTML = precoReais + " BRL"
+
+                    //Chamando a função que vai incrementar o preco em reais na url
+                    precoUrl(precoReais);
 
                 })
 
@@ -60,4 +59,9 @@ if (window.location.pathname === '/compraLoja.html') {
     }
 }
 
-
+function precoUrl(preco) {
+    //Incrementando o preco fornecido na URL
+    const url = new URL(window.location.href);
+    url.searchParams.set('precoReais', preco);
+    history.pushState(null, null, url);
+}
